b57f8a42c31392bfc30e49731bcbe602b30e5105
tanyakelly <murenkot@gmail.com>
2019-11-20 12:17:51 -0800

    initial setup

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers');
+
+// PATH = /api/v1/auth
+
+// router.post('/register', ctrl.auth.register);
+// router.post('/login', ctrl.auth.login);
+// router.get('/verify', ctrl.auth.verify);
+// router.post('/logout', ctrl.auth.logout);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const routes = require('./routes');
+
+// --------------------------------- Middleware --------------------------------- //
+
+// CORS - Cross Origi Resource Sharing
+const corsOptions = {
+  origin: [`http://localhost:3000`],
+  credentials: true, // allows the session cookie to be sent back and forth from server to client
+  optionsSuccessStatus: 200 // some legacy browsers choke on satus 204
+};
+
+app.use(cors(corsOptions));
+
+// BodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Express Session - Authentication
+
+app.use(session({
+  // Store session in DB
+  store: new MongoStore({ url: process.env.MONGO_URI }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false, // Only create session if a propery has been added to session,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 * 2,  // Expire at 2 weeks
+  }
+}));
+
+// ----------------------------------- Routes ----------------------------------- //
+
+app.get('/', (req, res) => {
+  res.send('<h1>AUTH API</h1>');
+});
+
+
+app.listen(PORT, () => console.log(`Server connected at http://localhost:${PORT}`));
