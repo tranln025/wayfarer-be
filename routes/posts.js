6580bb2e6b6c3e2f19a9b5489fa9c5ac73a6690e
JSiow <julian.k.siow@gmail.com>
2019-11-21 11:10:45 -0800

    adds showAll and show route for posts, show route is untested and still needs to recieve the id data properly from the front end

---
+++ routes/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers')
+
+//PATH = /api/v1/posts
+
+//GET All Posts
+router.get('/all', ctrl.posts.showAll);
+router.get('/:id', crtl.posts.show);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -45,5 +45,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/auth', routes.auth);
 app.use('/api/v1/users', routes.users);
+app.use('/api/v1/posts', routes.posts);
 
 app.listen(PORT, () => console.log(`Server connected at http://localhost:${PORT}`));
