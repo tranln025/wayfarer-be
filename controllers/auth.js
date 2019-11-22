428ee3061215b1bb0c9f5b31e73069ba3c0b38ac
ltran <tranln025@gmail.com>
2019-11-22 11:12:01 -0800

    Adds testing routes to get all and delete all users

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,6 +30,7 @@ const register = (req, res) => {
                 db.User.create(newUser, (err, savedUser) => {
                     if (err) return res.status(500).json({ status: 500, message: err});
                     res.status(201).json({ status: 201, data: savedUser, message: 'success' });
+                    req.session.currentUser = { id: savedUser._id };
                 });
             });
         });
