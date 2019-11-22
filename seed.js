ea2907ebec051891ae0e94c59cdab1b79fc8e8f2
AliStoltz <ali.mstoltz@gmail.com>
2019-11-22 11:41:42 -0800

    adds seed file

---
+++ seed.js
@@ -0,0 +1,26 @@
+const seed = [
+  {
+      comments: [],
+      city: "5dd729f834359bfe2b69b3c7",
+      title: "julian sux",
+      photo: "https://www.usnews.com/dims4/USNEWS/3e2b4ba/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fcom-usnews-beam-media.s3.amazonaws.com%2Fbb%2Fc8%2F59c40cdd4cacbc74086e09589bff%2F1064-the-bay-lights-3-james-ewing.jpg",
+      author: "5dd6c638f584bbe855228843",
+      
+  },
+  {
+      comments: [],
+      city: "5dd729f834359bfe2b69b3c7",
+      title: "ali doesntsux",
+      photo: "https://www.usnews.com/dims4/USNEWS/3e2b4ba/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fcom-usnews-beam-media.s3.amazonaws.com%2Fbb%2Fc8%2F59c40cdd4cacbc74086e09589bff%2F1064-the-bay-lights-3-james-ewing.jpg",
+      author: "5dd6c638f584bbe855228843",
+    
+  },
+  {
+      comments: [],
+      city: "5dd81bf863afbe028c0063dd",
+      title: "London Summer",
+      photo: "https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg",
+      author: "5dd6c638f584bbe855228843",
+     
+  }
+]
