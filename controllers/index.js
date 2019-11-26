e5b4e0574d0f3d15c2f7f6d52797515a57e1d444
ltran <tranln025@gmail.com>
2019-11-25 22:14:48 -0800

    Adds comments mvc

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ module.exports = {
     auth: require('./auth'),
     users: require('./users'),
     posts: require('./posts'),
-    cities: require('./cities')
+    cities: require('./cities'),
+    comments: require('./comments'),
   };
   
--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.use('/api/v1/auth', routes.auth);
 app.use('/api/v1/users', routes.users);
 app.use('/api/v1/posts', routes.posts);
 app.use('/api/v1/cities', routes.cities);
-
+app.use('/api/v1/comments', routes.comments);
 
 app.listen(PORT, () => console.log(`Server connected at http://localhost:${PORT}`));
